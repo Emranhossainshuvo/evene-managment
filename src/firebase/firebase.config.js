f0fcb66a3a96c82fad2c94cd1145a3158b250c05
emran <shuvoorba28@gmail.com>
2023-10-07 09:36:42 +0600

    initial set up done: react router app, tailwind css, daisyUI, firebase configaration, etc.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBlfxMSHU1qTmJ1mo_WYq3AcS4_w3w-dh0",
+  authDomain: "yoga-and-fitness-96e13.firebaseapp.com",
+  projectId: "yoga-and-fitness-96e13",
+  storageBucket: "yoga-and-fitness-96e13.appspot.com",
+  messagingSenderId: "427956001522",
+  appId: "1:427956001522:web:170fa7308bead0aa66ce6b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
