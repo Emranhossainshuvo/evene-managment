c842f98669bc9aae32af1169f06689b4a34b8fe6
emran <shuvoorba28@gmail.com>
2023-10-08 07:52:20 +0600

    footer added

--- src/components/pages/Home/Carts.jsx
+++ src/components/pages/Home/Carts.jsx
@@ -4,13 +4,13 @@ const Carts = ({ cards }) => {
     const { id, img, name, price, short_description } = cards;
 
     return (
-        <div className="card bg-base-100 shadow-xl image-full">
+        <div className="card bg-base-100 shadow-xl p-4 image-full">
             <figure><img className="" src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
                 <p>{short_description}</p>
+                <div className="card-actions items-center justify-end">
                 <p className="text-end">${price}</p>
-                <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
             </div>
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import Carts from "./Carts";
+import Footer from "./Footer";
 
 const Home = () => {
     const cards = useLoaderData();
     return (
         <div>
             <Banner></Banner>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 mb-10 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
                     cards.map(card => <Carts key={card.id} cards={card}></Carts>)
                 }
             </div>
+            <Footer></Footer>
         </div>
     );
 };
